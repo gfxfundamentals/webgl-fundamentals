2b231e3c4d675e6df206934ebd7c1557258559d0
Gregg Tavares <github@greggman.com>
2014-02-22 23:31:04 -1000

    add makeIdentiy and makeTranspose

--- webgl/resources/webgl-3d-math.js
+++ webgl/resources/webgl-3d-math.js
@@ -64,6 +64,24 @@ function cross(a, b) {
           a[0] * b[1] - a[1] * b[0]];
 }
 
+function makeIdentity() {
+  return [
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+  ];
+}
+
+function makeTranspose(m) {
+  return [
+    m[0], m[4], m[8], m[12],
+    m[1], m[5], m[9], m[13],
+    m[2], m[6], m[10], m[14],
+    m[3], m[7], m[11], m[15],
+  ];
+}
+
 function makePerspective(fieldOfViewInRadians, aspect, near, far) {
   var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
   var rangeInv = 1.0 / (near - far);
