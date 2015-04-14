1ea00cc36c9be48c97935a8f9ac243a879354217
Gregg Tavares <github@greggman.com>
2015-04-14 15:43:35 +0900

    update docs to new template

--- docs/scripts/toc.js
+++ docs/scripts/toc.js
@@ -55,16 +55,21 @@ $.fn.toc = function(options) {
       headingOffsets.push($h.offset().top - opts.highlightOffset);
 
       //add anchor
-      var anchor = $('<span/>').attr('id', opts.anchorName(i, heading, opts.prefix)).insertBefore($h);
+      if (opts.addAnchor !== false) {
+        var anchor = $('<span/>').attr('id', opts.anchorName(i, heading, opts.prefix)).insertBefore($h);
+      }
 
       //build TOC item
       var a = $('<a/>')
-        .text(opts.headerText(i, heading, $h))
-        .attr('href', '#' + opts.anchorName(i, heading, opts.prefix))
-        .bind('click', function(e) { 
-          scrollTo(e);
-          el.trigger('selected', $(this).attr('href'));
-        });
+        .html(opts.headerText(i, heading, $h))
+        .attr('href', opts.anchorName(i, heading, opts.prefix, '#'));
+
+      if (opts.goOnClick !== false) {
+          a.bind('click', function(e) {
+            scrollTo(e);
+            el.trigger('selected', $(this).attr('href'));
+          });
+      }
 
       var li = $('<li/>')
         .addClass(opts.itemClass(i, heading, $h, opts.prefix))
@@ -77,24 +82,59 @@ $.fn.toc = function(options) {
 };
 
 
-jQuery.fn.toc.defaults = {
+var indexOpts = {
+  container: '.dropdown-menu ',
+  selectors: 'li,ul',
+  smoothScrolling: true,
+  goOnClick: false,
+  prefix: 'toc',
+  onHighlight: function() {},
+  highlightOnScroll: false,
+  highlightOffset: 100,
+  addAnchor: false,
+  anchorName: function(i, heading, prefix) {
+    return $(heading).find("a").attr('href');
+  },
+  headerText: function(i, heading, $heading) {
+    return $heading.attr("id") || $heading.text();
+  },
+  itemClass: function(i, heading, $heading, prefix) {
+    return prefix + '-' + $heading[0].tagName.toLowerCase();
+  }
+};
+
+var normalOpts = {
   container: 'body',
-  selectors: 'h1,h2,h3',
+  selectors: 'h1,h2,h3,h4',
   smoothScrolling: true,
   prefix: 'toc',
   onHighlight: function() {},
   highlightOnScroll: true,
   highlightOffset: 100,
-  anchorName: function(i, heading, prefix) {
-    return prefix+i;
+  anchorName: function(i, heading, prefix, mark) {
+    return (mark || '') + ($( heading ).attr( "id" ) || ( prefix + i ));
   },
   headerText: function(i, heading, $heading) {
-    return $heading.text();
+    return $heading.text()
+                  .replace(/\</g, '&lt;')
+                  .replace(/\(/g, '<span class="toc-params"> (')
+                  .replace(/\)/g, ')</span>');
+    //return $heading.attr("id") || $heading.text();
   },
   itemClass: function(i, heading, $heading, prefix) {
     return prefix + '-' + $heading[0].tagName.toLowerCase();
   }
-
 };
 
+function isIndex() {
+  var path = window.location.pathname
+  return path.indexOf("index.html") >= 0 ||
+         path.substr(-1) === "/";
+}
+
+var opts = isIndex() ? indexOpts : normalOpts
+jQuery.fn.toc.defaults = opts;
+
 })(jQuery);
+
+
