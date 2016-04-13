0dc118e42244ef3b459199ff49b661902c52ebe4
Gregg Tavares <github@greggman.com>
2016-04-14 00:06:11 +0900

    fix webgl-utils

--- webgl/resources/webgl-utils.js
+++ webgl/resources/webgl-utils.js
@@ -259,7 +259,7 @@
       gl.attachShader(program, shader);
     });
     if (opt_attribs) {
-      opt_attrib.forEach(function(attrib, ndx) {
+      opt_attribs.forEach(function(attrib, ndx) {
         gl.bindAttribLocation(
             program,
             opt_locations ? opt_locations[ndx] : ndx,
