97418d246ca4ce3711ac2ef0cba74ade5016abe3
Gregg Tavares <github@greggman.com>
2016-10-03 13:14:00 +0900

    add import for css

--- webgl/resources/editor.js
+++ webgl/resources/editor.js
@@ -92,9 +92,12 @@ function parseHTML(url, html) {
   var externalScriptRE = /<script\s*src\s*=\s*"(.*?)"\s*>\s*<\/script>/ig;
   var hasCanvasInCSSRE = /canvas/;
   var hasCanvasStyleInHTMLRE = /<canvas[^>]+?style[^>]+?>/;
+  var cssLinkRE = /<link ([^>]+?)>/;
+  var isCSSLinkRE = /type="text\/css"|rel="stylesheet"/;
+  var hrefRE = /href="([^"]+)"/;
 
   var obj = { html: html };
-  htmlParts.css.source = getHTMLPart(styleRE, obj, '<style>${css}</style>');
+  htmlParts.css.source = getHTMLPart(styleRE, obj, '<style>\n${css}</style>');
   htmlParts.html.source = getHTMLPart(bodyRE, obj, '<body>${html}</body>');
   htmlParts.js.source = getHTMLPart(inlineScriptRE, obj, '<script>${js}</script>');
   html = obj.html;
@@ -106,8 +109,13 @@ function parseHTML(url, html) {
   });
 
   htmlParts.html.source += scripts + '\n';
-  g.html = html;
 
+  // add style section if there is non
+  if (html.indexOf("${css}") < 0) {
+    html = html.replace("</head>", "<style>\n${css}</style>\n</head>");
+  }
+
+  // add css if there is none
   if (!hasCanvasInCSSRE.test(htmlParts.css.source) && !hasCanvasStyleInHTMLRE.test(htmlParts.html.source)) {
     htmlParts.css.source = `body {
   margin: 0;
@@ -119,6 +127,23 @@ canvas {
 }
 ` + htmlParts.css.source;
   }
+
+  var links = '';
+  html = html.replace(cssLinkRE, function(p0, p1) {
+    if (isCSSLinkRE.test(p1)) {
+      var m = hrefRE.exec(p1);
+      if (m) {
+        links += `@import url("${m[1]}");\n`;
+      }
+      return '';
+    } else {
+      return p0;
+    }
+  });
+
+  htmlParts.css.source = links + htmlParts.css.source;
+
+  g.html = html;
 }
 
 function cantGetHTML(e) {
