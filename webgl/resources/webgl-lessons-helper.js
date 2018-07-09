1e3295d4fe7776f137cf64e2a5d668afdc3ff124
Travis CI <github@greggman.com>
2018-07-09 07:18:05 +0000

    Deploy to GitHub Pages: 37aa42f05eda2407468dfb0ac402426503b740b6

--- webgl/resources/webgl-lessons-helper.js
+++ webgl/resources/webgl-lessons-helper.js
@@ -118,6 +118,7 @@
   function setupConsole() {
     var parent = document.createElement("div");
     parent.className = "console";
+
     var numLinesRemaining = 100;
     var added = false;
 
@@ -268,14 +269,10 @@
     topWindow.requestAnimationFrame = (function(oldRAF) {
 
       return function(callback, element) {
-        var handler = function() {
-          if (isOnScreen(element)) {
-            oldRAF(callback, element);
-          } else {
-            oldRAF(handler, element);
-          }
+        const handler = function() {
+          return oldRAF(isOnScreen(element) ? callback : handler, element);
         };
-        handler();
+        return handler();
       };
 
     }(topWindow.requestAnimationFrame));
