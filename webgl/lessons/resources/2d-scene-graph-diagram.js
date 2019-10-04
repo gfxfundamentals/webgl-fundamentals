b7b5af16cba08164825fcf3c469dba8d154f145a
Gregg Tavares <github@greggman.com>
2019-10-04 19:16:20 +0900

    dark mode

--- webgl/lessons/resources/2d-scene-graph-diagram.js
+++ webgl/lessons/resources/2d-scene-graph-diagram.js
@@ -11,6 +11,15 @@ function duplicate(src) {
   return d;
 }
 
+const darkColors = {
+  arrow: '#DDD',
+};
+const lightColors = {
+  arrow: '#000',
+};
+const darkMatcher = window.matchMedia("(prefers-color-scheme: dark)");
+let colors;
+
 function renderSceneGraph(root) {
   const canvas = document.getElementById("c");
   const ctx = wrapCanvasRenderingContext2D(canvas.getContext("2d"));
@@ -188,6 +197,8 @@ function renderSceneGraph(root) {
   }
 
   function drawScene() {
+    const isDarkMode = darkMatcher.matches;
+    colors = isDarkMode ? darkColors : lightColors;
     webglUtils.resizeCanvasToDisplaySize(ctx.canvas);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.save();
@@ -197,6 +208,7 @@ function renderSceneGraph(root) {
     ctx.font = "12px sans-serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+    ctx.strokeStyle = colors.arrow;
     drawArrow(root);
     drawNode(root);
     ctx.restore();
