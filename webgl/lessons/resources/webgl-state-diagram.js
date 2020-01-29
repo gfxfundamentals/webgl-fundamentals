36622b76ae924c8511db36682acae718e9cc07b7
Gregg Tavares <github@greggman.com>
2020-01-30 06:25:32 +0900

    no webgl

--- webgl/lessons/resources/webgl-state-diagram.js
+++ webgl/lessons/resources/webgl-state-diagram.js
@@ -43,6 +43,12 @@ export default function main({webglVersion, windowPositions}) {
   hljs.initHighlightingOnLoad();
 
   gl = document.querySelector('canvas').getContext(webglVersion, {preserveDrawingBuffer: true});  /* eslint-disable-line */
+
+  if (!gl) {
+    document.body.classList.add('no-webgl');
+    return;
+  }
+
   twgl.addExtensionsToContext(gl);
 
   const diagramElem = document.querySelector('#diagram');
