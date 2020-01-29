b5a91dcfcf0776ba5571c4b9912b232ea2edc5b8
Travis CI <github@greggman.com>
2020-01-29 17:04:45 +0000

    Deploy to GitHub Pages: a759f97a21f86c69a1b751e2800235c08e88fa61

--- webgl/lessons/resources/webgl-state-diagram.js
+++ webgl/lessons/resources/webgl-state-diagram.js
@@ -13,6 +13,8 @@ import {
   createTemplate,
   updateElem,
   helpToMarkdown,
+  addRemoveClass,
+  createElem,
   addElem,
   flash,
   removeFlashes,
@@ -1080,15 +1082,107 @@ export default function main({webglVersion, windowPositions}) {
     };
   }
 
+  function isPowerOf2(value) {
+    return (value & (value - 1)) === 0;
+  }
+
   function createTextureDisplay(parent, name, texture) {
     const texElem = createTemplate(parent, '#texture-template');
     setName(texElem, name);
+    const nameLine = texElem.querySelector('.name-line');
+    const badElem = createElem('div', {
+      textContent: '☢',
+      className: 'bad',
+      dataset: {
+        help: isWebGL2
+            ? helpToMarkdown(`
+              <span style="color:red;">**This texture is un-renderable!**</span>
+
+              One or more of the following are true
+
+              * It as never been bound. A texture's type is unknown until bound at least once
+              * It has only 1 mip level that is larger than 1x1 but filtering is set to
+                use mips.
+              * It has a mip chain that doesn't match. Each lower mip is not half the width
+                and half the height of the previous mip or they are different internal formats.
+              * It doesn't have mips set to all specified mip levels.
+              * It's a --CUBE_MAP-- but it's not square or its sides are not the same size
+              `)
+            : helpToMarkdown(`
+              <span style="color:red;">**This texture is un-renderable!**</span>
+
+              One or more of the following are true
+
+              * It as never been bound. A texture's type is unknown until bound at least once
+              * It has only 1 mip level that is larger than 1x1 but filtering is set to
+                use mips.
+              * It's dimensions are not power-of-2 and has filtering on that requires mips
+                or has wrapping set to something other than --CLAMP_TO_EDGE--
+              * It has a mip chain that doesn't match. Each lower mip is not half the width
+                and half the height of the previous mip or they are different internal formats.
+              * It needs mips but doesn't have mips all the way down to 1x1 size.
+              * It's a --CUBE_MAP-- but it's not square or its sides are not the matching sizes
+              `),
+      }
+    });
+    nameLine.insertBefore(badElem, nameLine.lastChild);
 
     const mipsExpander = createExpander(texElem, 'mips');
     const mipsOuterElem = addElem('div', mipsExpander);
     const mipsElem = addElem('div', mipsOuterElem, {className: 'mips'});
-    const numMips = 8;
+    const maxMips = 8;
     const mips = [];
+
+    function isGood() {
+      const info = getWebGLObjectInfo(texture);
+      const target = info.target;
+      if (!target) {
+        return false;  // never bound
+      }
+
+      const firstMip = mips[0];
+      if (!firstMip) {
+        return false;  // no mips
+      }
+
+      let {width, height} = firstMip;
+      const isPOT = isWebGL2 || (isPowerOf2(width) && isPowerOf2(height));
+
+      const minFilter = gl.getTexParameter(target, gl.TEXTURE_MIN_FILTER);
+      const needsMips = minFilter !== gl.NEAREST && minFilter !== gl.LINEAR;
+      if (!isPOT && needsMips) {
+        return false;
+      }
+
+      if (!isPOT) {
+        const wrapS = gl.getTexParameter(target, gl.TEXTURE_WRAP_S);
+        const wrapT = gl.getTexParameter(target, gl.TEXTURE_WRAP_T);
+        if (wrapS !== gl.CLAMP_TO_EDGE || wrapS !== gl.CLAMP_TO_EDGE) {
+          return false;
+        }
+      }
+
+      if (needsMips) {
+        let level = 0;
+        for (;;) {
+          if (width === 1 && height === 1) {
+            break;
+          }
+          width = Math.max(width >> 1, 1);
+          height = Math.max(height >> 1, 1);
+          const mip = mips[++level];
+          if (!mip || mip.width !== width || mip.height !== height) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
+    function updateGood() {
+      addRemoveClass(badElem, 'good', isGood());
+    }
 
     function updateMips() {
       let size = 128;
@@ -1101,7 +1195,7 @@ export default function main({webglVersion, windowPositions}) {
       const magFilter = gl.getTexParameter(target, gl.TEXTURE_MAG_FILTER);
       const minFilter = gl.getTexParameter(target, gl.TEXTURE_MIN_FILTER);
 
-      for (let i = 0; i < numMips; ++i) {
+      for (let i = 0; i < maxMips; ++i) {
         const filter = i === 0
            ? magFilter === gl.LINEAR
            : minFilter === gl.NEAREST_MIPMAP_LINEAR || minFilter === gl.LINEAR_MIPMAP_LINEAR;
@@ -1165,6 +1259,7 @@ export default function main({webglVersion, windowPositions}) {
         });
         div.appendChild(mipCanvas);
       }
+      updateGood();
     }
 
     function updateMipFromData(target, level, internalFormat, width, height, border, format, type, data) {
@@ -1244,6 +1339,7 @@ export default function main({webglVersion, windowPositions}) {
       const info = getWebGLObjectInfo(texture);
       const target = info.target;
       const value = gl.getTexParameter(target, gl[pname]);
+      updateGood();
       return value;
     };
 
