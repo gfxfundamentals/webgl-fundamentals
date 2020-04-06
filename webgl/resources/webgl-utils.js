f317fbfb3e8f9fb39f33f209809dedd8b3d43811
Travis CI <github@greggman.com>
2020-04-06 12:24:00 +0000

    Deploy to GitHub Pages: f6ec7b1ccf6e87d57eb44f5f4a026037f2bc0fa8

--- webgl/resources/webgl-utils.js
+++ webgl/resources/webgl-utils.js
@@ -53,7 +53,7 @@
 
   if (!isInIFrame()) {
     console.log("%c%s", 'color:blue;font-weight:bold;', 'for more about webgl-utils.js see:');  // eslint-disable-line
-    console.log("%c%s", 'color:blue;font-weight:bold;', 'http://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html');  // eslint-disable-line
+    console.log("%c%s", 'color:blue;font-weight:bold;', 'https://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html');  // eslint-disable-line
   }
 
   /**
