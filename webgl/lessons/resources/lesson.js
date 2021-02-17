2afaa8e841c42f682bc3f033dfecc62a1ad2ee2c
greggman <github@greggman.com>
2021-02-17 13:27:22 +0000

    Deploying to gh-pages from  @ 83b2187e259a661707203cc1f5e5b7507b8249a1 🚀

--- webgl/lessons/resources/lesson.js
+++ webgl/lessons/resources/lesson.js
@@ -83,6 +83,10 @@ $(document).ready(function($) {
     window.location.href = this.value;
   });
 
+  $('a[data-href]').on('click', function() {
+    window.location.href = this.dataset.href;
+  });
+
 });
 }(jQuery));
 
