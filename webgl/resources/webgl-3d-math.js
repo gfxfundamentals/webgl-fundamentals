bd81125c599d3b48f8ab00bcb6b75f9143e84511
greggman <github@greggman.com>
2021-03-30 14:12:01 +0000

    Deploying to gh-pages from  @ 9448ee6682f7596a2f9de1aa4fda59e6074f0ae9 🚀

--- webgl/resources/webgl-3d-math.js
+++ webgl/resources/webgl-3d-math.js
@@ -836,7 +836,7 @@
     "Note: m4.multiply's multiplication order is reversed.",
     "In other words m4.multiply(a, b) needs to be m4.multiply(b, a)",
     "In math terms the old multiply multiplied the 2nd argument on the left of the first.",
-    "The new one multiplies the 2nd argument on the right of the the first.",
+    "The new one multiplies the 2nd argument on the right of the first.",
   ].join("\n"));
 
   return {
